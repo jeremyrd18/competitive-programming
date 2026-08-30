cd57dec4a6447026e44342eea41c7c4f68aed97d
jerwastaken <263959660+jerwastaken@users.noreply.github.com>
2026-08-30 13:44:11 +0700

    add

---
+++ codeforces/1854/b.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+const int N = 2e5 + 4;
+bitset<N> dp;
+int n, a[N];
+int ans, sum;
+signed main() {
+	cin >> n;
+	for (int i = 1; i <= n; ++i){
+        cin >> a[i];
+    }
+	dp[1] = 1;
+	for (int i = 1; i < (n << 1); ++i) {
+		sum += a[i];
+		if (dp[i]) ans = max(ans, sum - i + 1);
+		if (i <= n) {
+			dp[i - 1] = 0;
+			dp = dp | (dp << a[i]);
+		}
+	}
+	cout << ans;
+}
